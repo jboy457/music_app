74f1fe64b998422a6683eab5d3679bb8ad7c4f77
Taiwo Adejare <adejareemma@gmail.com>
2022-02-11 14:06:27 +0100

    app colors setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Colors.white;
+  static const Color brown = Color(0xFFC67D0F);
+  static const Color purple = Color(0xFF9570FF);
+  static const Color grey = Color(0xFF333333);
+  static const Color lightgrey = Color(0xFF828282);
+  static const Color green = Color(0xFF6FCF97);
+}
