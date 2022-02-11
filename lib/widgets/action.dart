0b6ed62cb1c03dd1687711fd007c1a616c67fe39
Taiwo Adejare <adejareemma@gmail.com>
2022-02-11 14:07:36 +0100

    song play action

---
+++ lib/widgets/action.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:music_app/constants/icons.dart';
+import 'package:music_app/constants/images.dart';
+
+class PlayAction extends StatelessWidget {
+  const PlayAction({
+    Key? key,
+    this.width,
+  }) : super(key: key);
+
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      padding: const EdgeInsets.only(
+        bottom: 15.0,
+        left: 15.0,
+        right: 15.0,
+      ),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              AppIcons.skipPrevios,
+            ),
+            CircleAvatar(
+                radius: 35,
+                backgroundColor: Colors.white,
+                backgroundImage: const AssetImage(AppImages.play),
+                child: SvgPicture.asset(AppIcons.start)),
+            SvgPicture.asset(
+              AppIcons.skipNext,
+            ),
+          ]),
+    );
+  }
+}
