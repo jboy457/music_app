1e4fee4902b561de0ee4b10d26ffc8b5946a97b5
Taiwo Adejare <adejareemma@gmail.com>
2022-02-11 14:06:47 +0100

    set app images

---
+++ lib/constants/images.dart
@@ -0,0 +1,15 @@
+class AppImages {
+  static const String profile = 'assets/images/profile.png';
+  static const String pointHand = 'assets/images/point_hand.png';
+  static const String avatar1 = 'assets/images/avatar1.png';
+  static const String avatar2 = 'assets/images/avatar2.png';
+  static const String song = 'assets/images/song.png';
+  static const String thumbnail1 = 'assets/images/thumbnail1.png';
+  static const String thumbnail2 = 'assets/images/thumbnail2.png';
+  static const String thumbnail3 = 'assets/images/thumbnail3.png';
+  static const String thumbnail4 = 'assets/images/thumbnail4.png';
+  static const String thumbnail5 = 'assets/images/thumbnail5.png';
+  static const String thumbnail6 = 'assets/images/thumbnail6.png';
+  static const String thumbnail7 = 'assets/images/thumbnail7.png';
+  static const String play = 'assets/images/play.png';
+}
