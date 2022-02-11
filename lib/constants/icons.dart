bd06b9f28394503c1c43df6b830a54ab2cbe89c9
Taiwo Adejare <adejareemma@gmail.com>
2022-02-11 14:06:40 +0100

    set app icons

---
+++ lib/constants/icons.dart
@@ -0,0 +1,8 @@
+class AppIcons {
+  static const String search = 'assets/icons/search.svg';
+  static const String start = 'assets/icons/start.svg';
+  static const String skipNext = 'assets/icons/skip-next.svg';
+  static const String skipPrevios = 'assets/icons/skip-previous.svg';
+  static const String lyrics = 'assets/icons/lyrics.svg';
+  static const String up = 'assets/icons/up.svg';
+}
